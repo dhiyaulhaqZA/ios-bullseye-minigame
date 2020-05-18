b74f88326de9b2551d0b797b4e09c099c22109dd
Dhiya Ulhaq Zulha Alamsyah <dhiya.alamsyah@students.amikom.ac.id>
2020-05-18 21:10:38 +0700

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Dhiya Ulhaq Zulha Alamsyah on 18/05/20.
+//  Copyright © 2020 dhiyaulhaqza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

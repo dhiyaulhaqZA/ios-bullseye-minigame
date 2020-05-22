16ab1d0885a2d02602be566fe2e670fa5c295ec1
Dhiya Ulhaq Zulha Alamsyah <dhiya.alamsyah@students.amikom.ac.id>
2020-05-23 00:32:29 +0700

    [Ulhaq] add text style

--- Bullseye/AboutView.swift
+++ Bullseye/AboutView.swift
@@ -21,11 +21,20 @@ struct AboutView: View {
         }
     }
     
+    struct TextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .font(Font.custom("Arial Rounded MT Bold", size: 16))
+                .foregroundColor(.black)
+                .padding(EdgeInsets(top: 0, leading: 60, bottom: 20, trailing: 60))
+        }
+    }
+    
     var body: some View {
         VStack {
             Text("🎯 Bullseye 🎯").modifier(HeadingStyle())
-            Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider")
-            Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.")
+            Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider").modifier(TextStyle())
+            Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.").modifier(TextStyle())
             Text("Enjoy!")
         }
         .navigationBarTitle("About Bullseye")

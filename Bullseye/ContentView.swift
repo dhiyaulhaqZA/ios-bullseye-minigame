fe879de4dd4b69a39ac67434652dd07da93882ad
Dhiya Ulhaq Zulha Alamsyah <dhiya.alamsyah@students.amikom.ac.id>
2020-05-22 14:47:35 +0700

    [Ulhaq] create bullseye view prototype

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsPresented = false
+    @State var sliderValue = 50.0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            HStack {
+                Text("Put the bullseye as close as you can:")
+                Text("100")
+            }
+            Spacer()
+            HStack {
+                Text("1")
+                Slider(value: $sliderValue, in: 1...100)
+                Text("100")
+            }
+            Spacer()
+            Button(action: {
+                self.alertIsPresented = true
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+            }.alert(isPresented: $alertIsPresented) { () -> Alert in
+                return Alert(title: Text("Hello there!"), message: Text(
+                    "The slider's value is 50.\n" +
+                    "You scored 999"), dismissButton: .default(Text("Awesome!"), action: {
+                    }))
+            }
+            Spacer()
+            HStack {
+                Button(action: {
+                    
+                }) {
+                    Text("Start over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Text("Info")
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }

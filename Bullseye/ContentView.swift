c6b0ebca0220cb7b6112823fb6241c524b7deadb
Dhiya Ulhaq Zulha Alamsyah <dhiya.alamsyah@students.amikom.ac.id>
2020-05-22 18:45:27 +0700

    [Ulhaq] add label style

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -16,18 +16,34 @@ struct ContentView: View {
     @State var score = 0
     @State var round = 1
     
+    struct LabelStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(.white)
+                .modifier(Shadow())
+                .font(Font.custom("Arial Rounded MT Bold", size: 18))
+        }
+    }
+    
+    struct Shadow: ViewModifier {
+        func body(content: Content) -> some View {
+            return content.shadow(color: .black, radius: 5, x: 2, y: 2)
+            
+        }
+    }
+    
     var body: some View {
         VStack {
             Spacer()
             HStack {
-                Text("Put the bullseye as close as you can:")
+                Text("Put the bullseye as close as you can:").modifier(LabelStyle())
                 Text("\(target)")
             }
             Spacer()
             HStack {
-                Text("1")
+                Text("1").modifier(LabelStyle())
                 Slider(value: $sliderValue, in: 1...100)
-                Text("100")
+                Text("100").modifier(LabelStyle())
             }
             Spacer()
             Button(action: {
@@ -51,10 +67,10 @@ struct ContentView: View {
                     Text("Start over")
                 }
                 Spacer()
-                Text("Score:")
+                Text("Score:").modifier(LabelStyle())
                 Text("\(score)")
                 Spacer()
-                Text("Round:")
+                Text("Round:").modifier(LabelStyle())
                 Text("\(round)")
                 Spacer()
                 Button(action: {
